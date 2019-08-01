cc3986d95e7a79a938e00961d34448dd07486fca
Brendan Bena <bzb98@live.com>
2019-08-01 17:15:04 -0600

    Update Python script
    Update gutentext_to_books.py script

--- gutentext_to_books.py
+++ gutentext_to_books.py
@@ -10,36 +10,61 @@ import itertools
 import gzip
 import json
 import pickle
+import os
 
-# Path to files
-path = 'pickles/'
-gutenpath = 'guten_text/'
+
+# Function that will create the necessary directory structure
+def dir_constructor():
+    
+    if os.path.exists('pickles/') == False:
+        os.mkdir('pickles/')
+        
 
 # Function to save a pickle of data
 def pickle_list(alist, name):
         with open(path + name + '.pkl', 'wb+') as file:
                 pickle.dump(alist, file, pickle.HIGHEST_PROTOCOL)
 
-# Create a list of lines from the gutenberg text
-lines = []
-for line in gzip.open(gutenpath+'gutenberg-poetry-v001.ndjson.gz'):
-    lines.append(json.loads(line.strip()))
 
-# Group poems on there 'GID' (unique number)
-#   essentially grouping by books
-poems_grouped = itertools.groupby(lines, key=lambda each: each['gid'])
+# Function that converts Allison Parrish's gutenberg corpus to a list of books
+#  then pickles the booklist for later use
+def guten_to_books():
 
-# List to hold strings of entire books
-book_list = []
+    # Path to directories
+    path = 'pickles/'
 
-# Loop through each poem book by id
-#   pull string value from dictionary
-#       create string of entire book
-for gid, groups in poems_grouped:
-    book_string = ''
-    for group in groups:
-        book_string += group['s'] + ' '
-    book_list.append(book_string)
+    # Create a list of lines from the gutenberg text
+    lines = []
+    for line in gzip.open('gutenberg-poetry-v001.ndjson.gz'):
+        lines.append(json.loads(line.strip()))
 
-# Save the pickle!!!
-pickle_list(book_list, 'booklist')
+    # Group poems on there 'GID' (unique number)
+    #   essentially grouping by books
+    poems_grouped = itertools.groupby(lines, key=lambda each: each['gid'])
+
+    # List to hold strings of entire books
+    book_list = []
+
+    # Loop through each poem book by id
+    #   pull string value from dictionary
+    #       create string of entire book
+    for gid, groups in poems_grouped:
+        book_string = ''
+        for group in groups:
+            book_string += group['s'] + ' '
+        book_list.append(book_string)
+
+    return book_list
+
+
+# Main Function
+if __name__ == '__main__':
+
+    # Path to directories
+    path = 'pickles/'
+
+    dir_constructor()
+    print('Creating list of books')
+    book_list = guten_to_books()
+    # Save the pickle!!!
+    pickle_list(book_list, 'booklist')
